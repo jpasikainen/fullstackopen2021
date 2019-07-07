f160dc3aa0497699821bfde5540582a914ad2ca8
jpasikainen <asikainen.juhapekka@gmail.com>
2019-07-07 14:41:18 +0300

    Part 2b

---
+++ osa2/phonebook/src/components/App.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('')
+
+  const addNumber = (event) => {
+    event.preventDefault()
+    if (persons.filter(person => person.name === newName).length > 0) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+    setPersons([...persons, {"name": newName, "number": newNumber}])
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilter = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const displayPersons = () => {
+    if (filter === '') {
+      return persons.map((person, key) => <p key={key}>{person.name} {person.number}</p>)
+    }
+    else {
+      return persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase())).map((person, key) => <p key={key}>{person.name} {person.number}</p>)
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={handleFilter}/>
+      </div>
+      <h2>add a new</h2>
+      <form onSubmit={addNumber}>
+        <div>
+          name: <input value={newName} onChange={handleNewName}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNewNumber}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {displayPersons()}
+    </div>
+  )
+}
+
+export default App
