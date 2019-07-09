a52ae9500bc79c408c313bdc74a5e02f1529610e
jpasikainen <asikainen.juhapekka@gmail.com>
2019-07-09 21:36:53 +0300

    Part 2b

--- osa2/countries/src/components/CountryInfo.js
+++ osa2/countries/src/components/CountryInfo.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Axios from 'axios'
 
 const CountryInfo = ({data, search}) => {
+    
+    const [temp, setTemp] = useState(0)
+    const [wind, setWind] = useState(0)
+
     const getLanguages = (languages) => {
         console.log(languages)
         return languages
@@ -10,6 +15,18 @@ const CountryInfo = ({data, search}) => {
                     </li>
                 )
     }
+
+    const getWeather = (location) => {
+        console.log(location)
+        Axios
+          .get(`http://api.apixu.com/v1/current.json?key=   &q=${location}`)
+          .then(response => {
+            console.log(response.data.current.temp_c)
+            setTemp(response.data.current.temp_c)
+            console.log(response.data.current.wind_kph)
+            setWind(response.data.current.wind_kph)
+          })
+      }
 
     return (
         data.filter(country => country.name.toLowerCase().includes(search))
@@ -23,6 +40,11 @@ const CountryInfo = ({data, search}) => {
                                 {getLanguages(country["languages"])}
                             </ul>
                             <img src={country.flag} alt="flag" style={{width: 200}}/>
+                            <h2>Weather in {country.capital}</h2>
+                            {getWeather(country.capital)}
+                            <p>temperature: {temp} Celsius</p>
+                            <p>wind: {wind} kph</p>
+                            <a href="https://www.apixu.com/" title="Weather API">Apixu.com</a>
                         </div>
                     )
     )
