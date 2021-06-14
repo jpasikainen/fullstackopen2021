00b8215d8d35c86514c6351935e67e27018e2c65
jpasikainen <jp.asikainen@pm.me>
2021-06-14 09:10:09 +0300

    Osa 2 C

--- osa2/countries/src/components/CountryInfo.js
+++ osa2/countries/src/components/CountryInfo.js
@@ -1,53 +1,55 @@
-import React, {useState} from 'react'
-import Axios from 'axios'
+import React, { useState } from "react";
+import Axios from "axios";
 
-const CountryInfo = ({data, search}) => {
-    
-    const [temp, setTemp] = useState(0)
-    const [wind, setWind] = useState(0)
+const api_key = process.env.REACT_APP_API_KEY;
 
-    const getLanguages = (languages) => {
-        console.log(languages)
-        return languages
-                .map((language, key) =>
-                    <li key={key}>
-                        {language.name}
-                    </li>
-                )
-    }
+const CountryInfo = ({ data, search }) => {
+  const [temp, setTemp] = useState(0);
+  const [iconURL, setIconURL] = useState("");
+  const [wind, setWind] = useState(0);
 
-    const getWeather = (location) => {
-        console.log(location)
-        Axios
-          .get(`http://api.apixu.com/v1/current.json?key=   &q=${location}`)
-          .then(response => {
-            console.log(response.data.current.temp_c)
-            setTemp(response.data.current.temp_c)
-            console.log(response.data.current.wind_kph)
-            setWind(response.data.current.wind_kph)
-          })
-      }
+  const getLanguages = (languages) => {
+    console.log(languages);
+    return languages.map((language, key) => <li key={key}>{language.name}</li>);
+  };
 
-    return (
-        data.filter(country => country.name.toLowerCase().includes(search))
-                    .map((country, key) =>
-                        <div key={key}>
-                            <h1>{country.name}</h1>
-                            <p>{country.capital}</p>
-                            <p>population {country.population}</p>
-                            <h2>languages</h2>
-                            <ul>
-                                {getLanguages(country["languages"])}
-                            </ul>
-                            <img src={country.flag} alt="flag" style={{width: 200}}/>
-                            <h2>Weather in {country.capital}</h2>
-                            {getWeather(country.capital)}
-                            <p>temperature: {temp} Celsius</p>
-                            <p>wind: {wind} kph</p>
-                            <a href="https://www.apixu.com/" title="Weather API">Apixu.com</a>
-                        </div>
-                    )
+  const getWeather = (location) => {
+    console.log(location);
+    Axios.get(
+      `http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${location}&aqi=no`
     )
-}
+      .then((response) => {
+        console.log(response.data.current.temp_c);
+        setTemp(response.data.current.temp_c);
+        console.log(response.data.current.wind_kph);
+        setWind(response.data.current.wind_kph);
+        setIconURL(response.data.current.condition.icon);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-export default CountryInfo
+  return data
+    .filter((country) => country.name.toLowerCase().includes(search))
+    .map((country, key) => (
+      <div key={key}>
+        <h1>{country.name}</h1>
+        <p>{country.capital}</p>
+        <p>population {country.population}</p>
+        <h2>languages</h2>
+        <ul>{getLanguages(country["languages"])}</ul>
+        <img src={country.flag} alt="flag" style={{ width: 200 }} />
+        <h2>Weather in {country.capital}</h2>
+        {getWeather(country.capital)}
+        <p>temperature: {temp} Celsius</p>
+        <img src={`https:${iconURL}`} />
+        <p>wind: {wind} kph</p>
+        <a href="https://www.weatherapi.com/docs/" title="Weather API">
+          weatherapi.com
+        </a>
+      </div>
+    ));
+};
+
+export default CountryInfo;
